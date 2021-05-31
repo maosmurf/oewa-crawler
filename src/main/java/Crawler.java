996ed2651296f24b1810d35dce6f7e080055f333
Adam Zielinski <github@maosmurf.com>
2021-05-31 14:44:42 +0200

    selectCountry

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Crawler {
@@ -10,6 +12,15 @@ public class Crawler {
 
     public void crawl(String url) {
         driver.get(url);
+        selectCountry();
+    }
+
+    private void selectCountry() {
+        var countriesValue = "CountryAUT";
+        var input  = driver.findElement(By.xpath("//input[@id=\"countries\"][@value=\"" + countriesValue + "\"]"));
+        input.click();
+        var form = driver.findElement(By.id("publicSearchForm"));
+        form.submit();
     }
 
     public void close() {
